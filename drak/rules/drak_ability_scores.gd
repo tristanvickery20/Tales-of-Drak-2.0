71f58ced6fb6c2ace29f2d2741b95535ca3f4153
tristanvickery20 <tristan.vickery20@gmail.com>
2026-04-29 22:17:13 -0500

    Add Drak ability scores rules resource

---
+++ drak/rules/drak_ability_scores.gd
@@ -0,0 +1,45 @@
+extends Resource
+class_name DrakAbilityScores
+
+@export var strength := 15
+@export var dexterity := 14
+@export var constitution := 13
+@export var intelligence := 12
+@export var wisdom := 10
+@export var charisma := 8
+
+func get_modifier(score: int) -> int:
+	return floori((float(score) - 10.0) / 2.0)
+
+func get_strength_modifier() -> int:
+	return get_modifier(strength)
+
+func get_dexterity_modifier() -> int:
+	return get_modifier(dexterity)
+
+func get_constitution_modifier() -> int:
+	return get_modifier(constitution)
+
+func get_intelligence_modifier() -> int:
+	return get_modifier(intelligence)
+
+func get_wisdom_modifier() -> int:
+	return get_modifier(wisdom)
+
+func get_charisma_modifier() -> int:
+	return get_modifier(charisma)
+
+func format_modifier(modifier: int) -> String:
+	if modifier >= 0:
+		return "+%d" % modifier
+	return str(modifier)
+
+func get_summary_text() -> String:
+	return "STR %d (%s)\nDEX %d (%s)\nCON %d (%s)\nINT %d (%s)\nWIS %d (%s)\nCHA %d (%s)" % [
+		strength, format_modifier(get_strength_modifier()),
+		dexterity, format_modifier(get_dexterity_modifier()),
+		constitution, format_modifier(get_constitution_modifier()),
+		intelligence, format_modifier(get_intelligence_modifier()),
+		wisdom, format_modifier(get_wisdom_modifier()),
+		charisma, format_modifier(get_charisma_modifier()),
+	]
